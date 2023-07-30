d54759fbf9d102bb56c8f70955190dccbcdfb2f9
dimka_sta <dimka.sta777@gmail.com>
2023-07-30 15:25:55 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+    <header className='header' >
+        <img alt='logo' src='https://upload.wikimedia.org/wikipedia/ru/thumb/e/e6/GAZ-Logo.svg/1200px-GAZ-Logo.svg.png' />
+    </header>
+    )
+    
+    
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+      <div className='content'>
+      <div>
+      <img  alt='logo' src='https://assets.kpmg.com/is/image/kpmg/future-car-1600-1:cq5dam.web.1082.378'/>
+      </div>
+      
+      <div>
+        ava
+      </div>
+
+      <div>
+        posts
+
+        <div>
+          new posts
+        </div>
+
+        <div>
+          
+          <div> 
+            post 1
+          </div>
+
+          <div>
+            post 2
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+    </div>
+    )
+        
+}
+
+export default Profile;
